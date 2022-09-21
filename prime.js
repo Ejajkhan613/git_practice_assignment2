7822e3a53973fba3522cadd33fe8fba7e8b59d6d
Ejajkhan613 <Lenovo@EJAJULANSARI.EJAJULANSARI>
2022-09-21 11:52:14 +0530

    This is the project

---
+++ prime.js
@@ -0,0 +1,13 @@
+let count = 0;
+let prime = 17;
+for(let a=1; a<=prime; a++) {
+
+if(prime%a==0){
+count++;
+}
+}
+
+if(count == 2) {
+console.log("prime");
+} else {
+  console.log("Not a Prime");
